fa61e69bcf365cae53a947dfa3824fc39017f681
Gabriel Alves <37886654+GabrielDragone@users.noreply.github.com>
2023-04-20 18:42:10 -0300

    Configurando projeto

---
+++ src/main/kotlin/br/com/gabrieldragone/mspedidos/MsPedidosApplication.kt
@@ -0,0 +1,13 @@
+package br.com.gabrieldragone.mspedidos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+
+@SpringBootApplication
+@EnableDiscoveryClient // Aplicação é um Cliente Eureka e deverá se registrar ao Eureka Server
+class MsPedidosApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsPedidosApplication>(*args)
+}
